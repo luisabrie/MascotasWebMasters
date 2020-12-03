96190a52687293a0f09ab01cd3c48b60ec6250da
lbperez <libruce1998hotmail.com>
2020-12-03 11:52:30 -0500

    Se agregó otro tipo de gráfico para otros datos

--- js/visitas.js
+++ js/visitas.js
@@ -1,4 +1,5 @@
 let visitas = [];
+let planes = [];
 
 
 let cargarJSON = () =>{
@@ -8,52 +9,92 @@ let cargarJSON = () =>{
     return resultado.json()
   })
   .then( (data) => {
-    
       for (let user of data.visitas){
         visitas.push(user.Enero + user.Febrero + user.Marzo + user.Abril + user.Mayo + user.Junio + user.Julio + user.Agosto+ user.Septiempre + user.Octubre + user.Noviembre+ user.Diciembre);
-      
-        //console.log(user.Enero);
     }
+  })
+  .catch( (error) => {
+    console.log("Error ",error)
+  })
+}
 
-      
 
+let cargarJSONPlan = () =>{
+  fetch("json/planes.json")
 
-
-
+  .then( (resultado) => {
+    return resultado.json()
+  })
+  .then( (data) => {
+    planes.push(data[0].estandar, data[0].premium, data[0].golden);
   })
 
   .catch( (error) => {
     console.log("Error ",error)
-
   })
   
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
 //Las funciones se ejecutarán en cuanto el documento esté completamente cargado.
 document.addEventListener('DOMContentLoaded', function() {
   cargarJSON();
+  cargarJSONPlan();
 })
 
 
 
 //let hacergraf = () =>{
 
-    var ctx = document.getElementById('chart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['2021', '2022', '2023'],
-            datasets: [{
-                label: 'Visitas',
-                backgroundColor: '#42a5f5',
-                borderColor: 'white',
-                data: visitas,
-            }
-        ]},
-        options: {responsive: true}
-    });
+var ctx = document.getElementById('chart').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['2021', '2022', '2023'],
+        datasets: [{
+            label: 'Visitas',
+            backgroundColor: '#42a5f5',
+            borderColor: 'white',
+            data: visitas,
+        }
+    ]},
+    options: {responsive: true}
+});
 
 
 //}
 
+var ctx2 = document.getElementById('chart2');
+
+var oilData2 = {
+    labels: [
+        "Estándar",
+        "Premium",
+        "Golden"
+    ],
+    datasets: [
+        {
+            data: planes,
+            backgroundColor: [
+                "#55A2DE",
+                "#26AD73",
+                "#E7E012"
+            ]
+        }]
+};
+
+var pieChart = new Chart(ctx2, {
+  type: 'pie',
+  data: oilData2
+});
