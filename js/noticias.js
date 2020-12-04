8d135c068efa12e6c99d029a856aa40c1aabcbf4
Bryan <brysloor@espol.edu.ec>
2020-12-03 22:05:47 -0500

    actualización, sección de noticias

--- js/noticias.js
+++ js/noticias.js
@@ -28,13 +28,11 @@ let cargarJSON=()=>{
     
     "</div>"
     destacado.appendChild(card)
-    //console.log(data)
       
       for (obj of data){
         
         card = document.createElement("div")
-
-        
+  
         titulo=obj.titulo
         cont=obj.contenido
         fecha= obj.fecha
@@ -44,15 +42,14 @@ let cargarJSON=()=>{
         card.setAttribute("class"," card col-lg-3  col-md-5 col-sm-10  ")
         card.style.width='20rem'
         card.innerHTML=
-        
-        
+   
         "<img class=\"card-img-top img-fluid\""+ 
         "src=\"https://img.webnots.com/2017/04/Bootstrap-Card-Image.png\" alt=\"card image\">"+
         "<div  name =\"Card\" class=\"card-body\">"+
         "<h4 class=\"card-title\">"+titulo+"</h4>"+
         "<div id=\"usuario\">"+usuario+"</div>"+
         "<p class=\"card-text\">"+cont+"</p>"+
-        "<div id=\"fecha\" class=\" float-right\">"+fecha+"</div>"+
+        "<div id=\"fecha\" class=\" float-right \">"+fecha+"</div>"+
         "<a  id=\"vermas\" href=\"#destacado\" class=\"btn btn-info btn-md\" onclick=\"manejarVista()\">Ver más</a>"+
 
         //"<button id=\"vermas\" class=\"btn btn-primary\" onclick=\"manejarVista()\" >Ver más</button>"+
@@ -61,7 +58,7 @@ let cargarJSON=()=>{
       }    
       
     })
-    //.then(()=>{manejarVista()})
+    .then((str)=>{filtro();})
     .catch( (error) => {
   
       console.log("Error ",error)
@@ -76,14 +73,15 @@ let manejarVista=()=>{
   let vista = document.getElementById("destacado")
   let contenido= vista.childNodes[1].childNodes[1].childNodes
 
-  console.log(contenido)
+  //console.log(contenido)
   for (var i = 0; i < news.length; i++){
     news[i].addEventListener('click', ev => {
+    
       let cont=ev.currentTarget.childNodes[1].childNodes
-      contenido[0].innerHTML=cont[0].textContent
-      contenido[1].innerHTML=cont[1].textContent
-      contenido[2].innerHTML=cont[2].textContent
-      contenido[3].innerHTML=cont[3].textContent
+      contenido[0].textContent=cont[0].textContent
+      contenido[1].textContent=cont[1].textContent
+      contenido[2].textContent=cont[2].textContent
+      contenido[3].textContent=cont[3].textContent
                     
   });
 
@@ -93,41 +91,44 @@ let manejarVista=()=>{
 
 
 
+let filtro =()=>{
+  
+  let input = document.getElementById("myInput")
+  //noticias= document.getElementsByClassName("card");
+  //arrCard=Array.from(noticias)
+  //console.log(noticias[0].childNodes[1].childNodes)
+
+input.onkeyup=function(){
+      var input, filter, noticias, contenidos;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      noticias= document.getElementsByClassName("card");
+      cont=noticias
+     console.log(noticias[0].childNodes[1].childNodes)
+      let i=0
+      for(let valor of noticias){
+        let card=noticias[i].childNodes[1].childNodes
+        let tit=card[0].textContent.toUpperCase().trim().includes(filter)
+        console.log(filter)
+        
+        if(tit){
+          valor.style.display=""
+
+        }else{
+          valor.style.display="none"
+        }
+        //console.log(user)
+
+        i++
+      }
+    }
+}
+ 
 
   
   document.addEventListener('DOMContentLoaded', function() {
     cargarJSON();
-    //filtro();
+    
     
   })
   
-  /*
-  let filtro =()=>{
-      let input = document.getElementById("myInput")
-  
-    input.onkeyup=function(){
-          var input, filter, tweets, contenidos;
-          input = document.getElementById("myInput");
-          filter = input.value.toUpperCase();
-          
-          tweets = document.getElementById("Tweets");
-          
-          contenidos=document.getElementsByName("grupoC")
-          let arrayContenidos= Array.from(contenidos)
-          let i=0;
-          console.log(arrayContenidos[0].textContent)
-  
-          for(let valor of arrayContenidos){
-              if(valor.textContent.toUpperCase().trim().includes(filter)){
-                valor.parentElement.style.display=""
-               
-              }else{
-                valor.parentElement.style.display="none"
-                
-              }
-         
-          }
-    } 
-  
-  
-  }*/
