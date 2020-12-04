44b09e5e8e43a886aecfb0fb862595e5395adc60
lbperez <libruce1998hotmail.com>
2020-12-03 23:20:42 -0500

    Pequeños arreglos

--- js/casos.js
+++ js/casos.js
@@ -4,37 +4,11 @@ let cargarJSON=()=>{
     .then(data => {
 
     let contenedor = document.getElementById('cartas')
-    /*let destacado = document.getElementById('destacado')
-    let titulo=data[0].titulo
-    let cont=data[0].contenido
-    let fecha= data[0].fecha
-    let usuario= data[0].usuario
-    let card = document.createElement("div")
-    
-    
-    /*card.setAttribute("id","vista")
-    card.setAttribute("class","  col-lg-10   col-sm-10 ")
-    card.style.width='20rem'
-    card.innerHTML=
-    
-    
-    "<img class=\"card-img img-fluid\""+ 
-    "src=\"https://img.webnots.com/2017/04/Bootstrap-Card-Image.png\" alt=\"card image\">"+
-    "<div class=\"card-body\">"+
-    "<h4 class=\"card-title\">"+titulo+"</h4>"+
-    "<div id=\"usuario\">"+usuario+"</div>"+
-    "<p class=\"card-text\">"+cont+"</p>"+
-    "<div id=\"fecha\" class=\" float-right\">"+fecha+"</div>"+
-    
-    "</div>"
-    destacado.appendChild(card)*/
-    //console.log(data)
-      
+
       for (obj of data){
         
         card = document.createElement("div")
 
-        
         nombre=obj.nombre
         raza=obj.raza
         sexo= obj.sexo
@@ -54,15 +28,13 @@ let cargarJSON=()=>{
         "<p class=\"card-text\">"+raza+"</p>"+
         "<div id=\"fecha\" class=\" float-right\">"+sexo+"</div>"+
 
-        //"<button id=\"vermas\" class=\"btn btn-primary\" onclick=\"manejarVista()\" >Ver más</button>"+
         "</div>"
         contenedor.appendChild(card)
       }    
       
     })
-    //.then(()=>{manejarVista()})
+
     .catch( (error) => {
-  
       console.log("Error ",error)
   
     })
